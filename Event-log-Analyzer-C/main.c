99950e7477e40d4a9600fba400ba3a783628ef3e
Unekwu <39774708+Unekwu@users.noreply.github.com>
2020-05-02 18:49:28 -0400

    Update main.c

--- Event-log-Analyzer-C/main.c
+++ Event-log-Analyzer-C/main.c
@@ -1,6 +1,6 @@
 
 //****************************************************************
-//Unekwu-Ojo's main.cpp file to read input log to track user activity
+//Unekwu-Ojo's main.c file to read input log to track user activity
 //****************************************************************
 #include "eventlog.h"
 #include <stdio.h>
@@ -13,15 +13,14 @@ char* concat(const char *s1, const char *s2)
     const size_t len1 = strlen(s1);
     const size_t len2 = strlen(s2);
     char *result = malloc(len1 + len2 + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
     memcpy(result, s1, len1);
-    memcpy(result + len1, s2, len2 + 1); // +1 to copy the null-terminator
+    memcpy(result + len1, s2, len2 + 1);
     return result;
 }
 
 void init() {
 	llist*  root = (llist*)malloc(sizeof(llist));// 
-	root->next = NULL;//constructor to vreate new node 
+	root->next = NULL;//constructor to create new node 
 					  //making curr point to root
 }
 
@@ -35,10 +34,10 @@ void createNewNode(llist* root) {
 
 int main() {
 	//init();
-	llist readLog;//instantiate an object
-  llist *head;
-	FILE* fin;//input file stream 
-	char move[512];//string to read the file content
+   llist readLog;//instantiate an object
+   llist *head;
+   FILE* fin;//input file stream 
+   char move[512];//string to read the file content
   char temp1 [512];//string to read the first piece of time
   char temp2 [512];//string to read the second piece of time
   char *time_result;// string to obtain the final time result
@@ -64,56 +63,40 @@ int main() {
 	printf("Date\t\t Time \t\t Event ID \t\t IDcategory\t\tTask Category\t\tsecurity ID\t\taccount name\t\tAccount domain\t\tlogon ID");
 
 	if (fin != NULL) {
-		//while (fgets(move, fSize, fin)) {
-		//while (fscanf(fin, "%s", move)) {
 		while (!feof(fin)) 
 		 {
        fgets(ignore, sizeof(ignore), fin);
-			// fscanf(fin, "%s", move);
-      //fscanf(fin,"%s", move );
-			//if not equal to information keep moving
       	fscanf(fin, "%s", move);
 			long int res = 11;
+			//while not equal to information keep moving
 			while (strcmp(move, "Information") != 0) {
 				fscanf(fin, "%s", move);
 			}
 			
-        fscanf(fin, "%s", move);
-				includeDate(&readLog, move);//add date to linkedlist
+			fscanf(fin, "%s", move);
+			includeDate(&readLog, move);//add date to linkedlist
 
-				fscanf(fin, "%s", move);
-        
-        strcpy(temp1, move);
-	//		includeTime(&readLog, move);//add time to linkedlist
-      //printf("temp1 %s", temp1);
-				fscanf(fin, "%s", move);
-       // printf("move %s", move);
-       
-         
-       strcpy(temp2, move);
-      //  printf("temp2 %s", temp2);
+			fscanf(fin, "%s", move);
+
+			strcpy(temp1, move);
+			fscanf(fin, "%s", move);
+
+		       strcpy(temp2, move);
+		 
 
       
        time_result =  concat(temp1,temp2);
        strcpy(move, time_result);
-       // printf("time_Result %s", move);
-				includeTime(&readLog, move);//add the other part of the time
-//*/
+       includeTime(&readLog, move);//add the other part of the time
        free(time_result);
         
-				fscanf(fin, "%s", move);
-        //printf("move before source %s", move);
-    
-				includeSource(&readLog, move);//add source to linkedlist
+	fscanf(fin, "%s", move);
+	includeSource(&readLog, move);//add source to linkedlist
+	fscanf(fin, "%s", move);
+	includeEventID(&readLog, move);//add eventID to linkedlist
+	includeEventIdCategory(&readLog, move);
 
-				fscanf(fin, "%s", move);
-   // printf("The event ID is %s", move);
-				includeEventID(&readLog, move);//add eventID to linkedlist
-				includeEventIdCategory(&readLog, move);
-
-				fscanf(fin, "%s", move);
-				//read until the literal has ended
-        //try while(move[strlen(move)]=0 )
+	fscanf(fin, "%s", move);
         strcpy(temp4, move);
        // printf("move before eventID:  %s\n", move);
         fscanf(fin, "%s", move);
@@ -121,33 +104,27 @@ int main() {
         eventCategory_result = concat(temp4, temp5);
        // printf("The event Category result is %s \n", eventCategory_result);
         includeEventCategory(&readLog, eventCategory_result);
-        /*
-				while (strcmp(&move[0], "\0") == 0) {
-					includeEventCategory(&readLog, move);//add event category to linkedlist
-					fscanf(fin, "%s", move);
-				}*/
-			
-			while (strcmp(move, "Subject:") != 0) {
-				fscanf(fin, "%s", move);
-			}
+	while (strcmp(move, "Subject:") != 0) {
+		fscanf(fin, "%s", move);
+	}
 
 			fscanf(fin, "%s", move);
 			fscanf(fin, "%s", move);
 			fscanf(fin, "%s", move);
-			includeSecID(&readLog, move);//read the secID into the list 
+	includeSecID(&readLog, move);//read the secID into the list 
 										 //nextLine(fin);
 			fscanf(fin, "%s", move);
 
 			fscanf(fin, "%s", move);
 			fscanf(fin, "%s", move);
 
-			includeAccName(&readLog, move);//read the account name into the list
+	includeAccName(&readLog, move);//read the account name into the list
 
 			fscanf(fin, "%s", move);
 			fscanf(fin, "%s", move);
 
 			fscanf(fin, "%s", move);
-			includeAccDomain(&readLog, move);//read account domain
+	includeAccDomain(&readLog, move);//read account domain
 											 //nextLine(fin);
 			fscanf(fin, "%s", move);
 			fscanf(fin, "%s", move);
@@ -157,28 +134,18 @@ int main() {
       
     
      displayToDiffFile(&readLog);
-			createNewNode(&readLog); //Creating a new node
+      createNewNode(&readLog); //Creating a new node
       printf("\n");		
       displaylogInfoToFile(&readLog);
       displaylogInfo(&readLog);
      
 		}
-    //displaylogInfo(&readLog);
+    
 	}
   else{
     printf("File not open\n");
   }
- fclose(fin);
- 
-    
-
-//printf("start of print to diff file \n");
-
-//close file 
-	//read the users logon information
-	//display info to screen 
-  //display info to file
-//display into different files  
-								//	system("pause");
-	return 0;
+ fclose(fin);				
+	
+return 0;
 }
